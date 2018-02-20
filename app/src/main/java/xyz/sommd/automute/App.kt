46d60ed3ece6ba821ec214e187b4868010701ebd
sommd <d.j.s.1050@gmail.com>
2018-02-21 03:15:27 +1100

    Add AutoMuteService and implement service starting/stopping

--- app/src/main/java/xyz/sommd/automute/App.kt
+++ app/src/main/java/xyz/sommd/automute/App.kt
@@ -1,9 +1,11 @@
 package xyz.sommd.automute
 
 import android.app.Application
+import android.content.Intent
+import xyz.sommd.automute.service.AutoMuteService
 import xyz.sommd.automute.settings.Settings
 
-class App: Application() {
+class App: Application(), Settings.ChangeListener {
     lateinit var settings: Settings
     
     override fun onCreate() {
@@ -11,5 +13,31 @@ class App: Application() {
         
         settings = Settings(this)
         settings.setDefaultValues()
+        settings.addChangeListener(this)
+        
+        if (settings.serviceEnabled) {
+            startAutoMuteService()
+        }
+    }
+    
+    override fun onSettingsChanged(settings: Settings, key: String) {
+        when (key) {
+            Settings.SERVICE_ENABLED_KEY -> {
+                if (settings.serviceEnabled) {
+                    startAutoMuteService()
+                } else {
+                    stopAutoMuteService()
+                }
+            }
+        }
+    }
+    
+    private fun startAutoMuteService() {
+        // TODO Use startForegroundService
+        startService(Intent(this, AutoMuteService::class.java))
+    }
+    
+    private fun stopAutoMuteService() {
+        stopService(Intent(this, AutoMuteService::class.java))
     }
 }
---
+++ app/src/main/java/xyz/sommd/automute/service/AutoMuteService.kt
@@ -0,0 +1,48 @@
+package xyz.sommd.automute.service
+
+import android.app.Service
+import android.content.Intent
+import android.media.AudioManager
+import android.media.AudioPlaybackConfiguration
+import android.os.Handler
+import android.os.IBinder
+import android.widget.Toast
+import androidx.content.systemService
+import xyz.sommd.automute.settings.Settings
+import xyz.sommd.automute.utils.AudioPlaybackMonitor
+
+class AutoMuteService: Service(), AudioPlaybackMonitor.Listener {
+    override fun onBind(intent: Intent?): IBinder? = null
+    
+    private lateinit var settings: Settings
+    private lateinit var handler: Handler
+    private lateinit var audioManager: AudioManager
+    private lateinit var playbackMonitor: AudioPlaybackMonitor
+    
+    override fun onCreate() {
+        Toast.makeText(this, "Starting Auto Mute Service", Toast.LENGTH_SHORT).show()
+        
+        settings = Settings.from(this)
+        handler = Handler()
+        audioManager = systemService()
+        playbackMonitor = AudioPlaybackMonitor(this, this)
+        
+        audioManager.registerAudioPlaybackCallback(playbackMonitor, handler)
+    }
+    
+    override fun onDestroy() {
+        Toast.makeText(this, "Stopping Auto Mute Service", Toast.LENGTH_SHORT).show()
+        
+        audioManager.unregisterAudioPlaybackCallback(playbackMonitor)
+    }
+    
+    override fun audioPlaybackStarted(config: AudioPlaybackConfiguration) {
+        // TODO
+        Toast.makeText(this, "Playback Started: " + config, Toast.LENGTH_SHORT).show()
+    }
+    
+    override fun audioPlaybackStopped(config: AudioPlaybackConfiguration) {
+        // TODO
+        Toast.makeText(this, "Playback Stopped: " + config, Toast.LENGTH_SHORT).show()
+    }
+}
